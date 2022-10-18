f5c65253e8d20b764da3458413409f6ccb0b653d
namho.kim <shawnbest@naver.com>
2022-10-18 16:41:41 +0900

    change

--- 소스.cpp
+++ 소스.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i < 9; i++) {
-		for (int k = 1; k < 9; k++) {
+	for (int i = 1; i < 10; i++) {
+		for (int k = 1; k < 10; k++) {
 			cout << i << " x " << k << " = " << i * k << endl;
 		}
 		cout << endl;
